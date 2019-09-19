cb8323d8bacd61c6640444cafc8349fef16f6dc6
damian-balas <damian.webdev@gmail.com>
2019-09-19 14:24:04 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -25,68 +25,43 @@ function throttle (fn, wait) {
 }
 
 // scroll function
-function scrollIt (destination, duration = 200, easing = 'linear', callback) {
-  const easings = {
-    linear (t) {
-      return t
-    },
-    easeInQuad (t) {
-      return t * t
-    },
-    easeOutQuad (t) {
-      return t * (2 - t)
-    }
+const easeInCubic = function (t) { return t * (2 - t) }
+const scrollToElem = (startTime, currentTime, duration, scrollEndElemTop, startScrollOffset) => {
+  const runtime = currentTime - startTime
+  let progress = runtime / duration
+  progress = Math.min(progress, 1)
+  const ease = easeInCubic(progress)
+  window.scroll(0, startScrollOffset + (scrollEndElemTop * ease))
+  if (runtime < duration) {
+    requestAnimationFrame((timestamp) => {
+      const currentTime = timestamp || new Date().getTime()
+      scrollToElem(startTime, currentTime, duration, scrollEndElemTop, startScrollOffset)
+    })
   }
-
-  const start = window.pageYOffset
-  const startTime = 'now' in window.performance ? performance.now() : new Date().getTime()
-
-  const documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight)
-  const windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight
-  const destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop - 120
-  const destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset)
-
-  if ('requestAnimationFrame' in window === false) {
-    window.scroll(0, destinationOffsetToScroll)
-    if (callback) {
-      callback()
-    }
-    return
-  }
-
-  function scroll () {
-    const now = 'now' in window.performance ? performance.now() : new Date().getTime()
-    const time = Math.min(1, ((now - startTime) / duration))
-    const timeFunction = easings[easing](time)
-    window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start))
-
-    if (window.pageYOffset === destinationOffsetToScroll) {
-      if (callback) {
-        callback()
-      }
-      return
-    }
-
-    requestAnimationFrame(scroll)
-  }
-
-  scroll()
 }
-const scrollLinks = document.querySelectorAll('a[href^="#"]')
 
-scrollLinks.forEach(scrollLink => scrollLink.addEventListener('click', (e) => {
-  e.preventDefault()
-  scrollIt(
-    document.querySelector(scrollLink.getAttribute('href')),
-    900,
-    'easeOutQuad'
-  )
-}))
+const scrollLinks = document.querySelectorAll('a[href^="#"]')
+scrollLinks.forEach(scrollLink => {
+  scrollLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    const scrollEndElem = document.querySelector(scrollLink.getAttribute('href'))
+    const anim = requestAnimationFrame((timestamp) => {
+      const stamp = timestamp || new Date().getTime()
+      const duration = 700
+      const offset = 100
+      const start = stamp
+      const startScrollOffset = window.pageYOffset
+      const scrollEndElemTop = scrollEndElem.getBoundingClientRect().top - offset
+
+      scrollToElem(start, stamp, duration, scrollEndElemTop, startScrollOffset)
+    })
+  })
+})
 
 // hide preloader
 setTimeout(() => {
   preloader.style.display = 'none'
-}, 5500)
+}, 553300)
 
 // Hamburger line color change
 function changeHamburgerLineColor () {
@@ -110,23 +85,6 @@ function changeHamburgerLineColor () {
   }
   if (contactDistanceToTopMinusHamburger > 0 && scrollY > aboutMeOffsetTop + aboutMeHeight && !hasClass) {
     hamburgerBtn.classList.add('blackline')
-  }
-}
-
-function mobilePortfolio () {
-  const windowHeight = window.innerHeight
-  const portfolioDistanceToTop = portfolio.getBoundingClientRect().top
-  if (window.innerWidth <= 786) {
-    if (portfolioDistanceToTop + windowHeight * 0.4 < windowHeight) {
-      projects.forEach((project, index) => setTimeout(() => {
-        project.classList.add('show-text')
-      }, 300 * index))
-    }
-  }
-  if (window.innerWidth > 786 || portfolioDistanceToTop + windowHeight * 0.4 > windowHeight) {
-    projects.forEach((project, index) => setTimeout(() => {
-      project.classList.remove('show-text')
-    }, 300 * index))
   }
 }
 
@@ -164,7 +122,6 @@ function animateItems () {
 }
 
 function callScrollFunctions () {
-  mobilePortfolio()
   changeHamburgerLineColor()
   animateItems()
 }
